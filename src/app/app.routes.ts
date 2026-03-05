0c08eabba948f512564795ff4bee367bfc9584bd
AlcedoEllyn <alcedoellyn22@gmail.com>
2026-03-05 20:54:00 +0700

    Configured the app-routes and app-ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,38 @@
+import { Interpolation } from './comps/interpolation/interpolation';
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { PropertyBinding } from './comps/property/property';
+import { EventBinding } from './comps/event/event';
+import { Home } from './comps/home/home';
+import { TwoWayBinding } from './comps/two-way/two-way';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: 'interpolation',
+        component: Interpolation,
+    },
+
+    {
+        path: 'property-binding',
+        component: PropertyBinding
+    },
+
+    {
+        path: 'event-binding',
+        component: EventBinding
+    },
+
+    {
+        path: 'home',
+        component: Home
+    },
+
+     {
+        path: 'two-way-binding',
+        component: TwoWayBinding
+    },
+
+   
+];
+
