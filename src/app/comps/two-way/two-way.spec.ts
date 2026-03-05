96f7a392b2b8d2560d6fb86b4705ce23b032e3f2
AlcedoEllyn <alcedoellyn22@gmail.com>
2026-03-05 20:50:47 +0700

    Added the html and ts codes from the given notes

--- src/app/comps/two-way/two-way.spec.ts
+++ src/app/comps/two-way/two-way.spec.ts
@@ -1,23 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TwoWayBinding } from './two-way';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
-import { TwoWay } from './two-way';
-
-describe('TwoWay', () => {
-  let component: TwoWay;
-  let fixture: ComponentFixture<TwoWay>;
+describe('TwoWayBinding', () => {
+  let component: TwoWayBinding;
+  let fixture: ComponentFixture<TwoWayBinding>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TwoWay]
+      imports: [TwoWayBinding, FormsModule, CommonModule]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(TwoWay);
+    fixture = TestBed.createComponent(TwoWayBinding);
     component = fixture.componentInstance;
-    await fixture.whenStable();
+    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have default name "Juan Dela Cruz"', () => {
+    expect(component.twoWName).toBe('Juan Dela Cruz');
+  });
+
+  it('should clear name when resetForm is called', () => {
+    component.twoWayName = 'Temporary Name';
+    component.resetForm();
+    expect(component.twoWayName).toBe('');
   });
 });
