223ae948086981307579b7493aacd3d66cdb77ba
AlcedoEllyn <alcedoellyn22@gmail.com>
2026-03-05 20:35:57 +0700

    Created the navigation component and the 4 binding types

---
+++ src/app/comps/two-way/two-way.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-two-way',
+  imports: [],
+  templateUrl: './two-way.html',
+  styleUrl: './two-way.css',
+})
+export class TwoWay {
+
+}
